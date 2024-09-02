0fe077d72b735b4e60c2a79c26dbbbb5558646a0
CAN ATA <connectwiththeuniverse1@gmail.com>
2024-09-02 17:54:03 +0300

    Update App.js

--- connect-with-the-universe/src/App.js
+++ connect-with-the-universe/src/App.js
@@ -1,42 +1,16 @@
 import './App.css';
-import React, { useReducer } from 'react';
+import React from 'react';
+import Word from './contexts/Word';
 
 
 
-function CustomFunction() {
-  const initialState = 0;
-
-  function reducer(state, action) {
-    switch (action.type) {
-      case 'increment':
-        return state + 1;
-      case 'decrement':
-        return state - 1;
-      default:
-        throw new Error();
-    }
-  }
-
-  const [state, dispatch] = useReducer(reducer, initialState);
+function App() {
   return (
     <>
-      Count: {state}
-      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
-      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+      <Word />
     </>
   );
 }
 
-
-function App() {
-
-  return (
-    <div className="App" >
-      <h1>React Application</h1>
-      <CustomFunction />
-    </div>
-  );
-
-}
 
 export default App;

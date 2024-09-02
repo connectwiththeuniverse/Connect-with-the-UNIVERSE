657b502a6138ab659f834d7f7efdd0ab4f49f7d3
CAN ATA <connectwiththeuniverse1@gmail.com>
2024-09-02 23:42:04 +0300

    Update Word.js

--- connect-with-the-universe/src/contexts/Word.js
+++ connect-with-the-universe/src/contexts/Word.js
@@ -5,16 +5,52 @@ import React, { useReducer } from 'react';
 
 
 function Word() {
-
-
-  
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  const toggleMenu = () => {
+    dispatch({ type: 'TOGGLE_MENU' });
+  };
+
+  const handleInputChange = (event) => {
+    dispatch({ type: 'SET_WORD', payload: event.target.value });
+  };
+
+  const generateSentences = () => {
+    dispatch({ type: 'GENERATE_SENTENCES' });
+  };
+
   return (
-    <>
-      Count: {state}
-      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
-      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
-    </>
+    <div className="App">
+      <div className="middledoc">
+        <div className="grid-container">
+          <div className="grid-item">
+            <div className="flex-container">
+              <div className="flex-item">
+                Sunucu Modülü: <br />
+                Methodology: <br />
+                <div>
+                  <input
+                    type="text"
+                    value={state.word}
+                    onChange={handleInputChange}
+                  />
+                  <button onClick={generateSentences}>Cümle Oluştur</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="grid-container">
+          {state.sentences.map((sentence, index) => (
+            <div className="grid-item" key={index}>
+              <div className="flex-container">
+                <div className="flex-item">{sentence}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
 

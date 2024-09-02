35fe048250855697cdac834ecafdfc1cd103e2a5
CAN ATA <connectwiththeuniverse1@gmail.com>
2024-09-02 23:41:44 +0300

    Update WordReducer.js

--- connect-with-the-universe/src/contexts/WordReducer.js
+++ connect-with-the-universe/src/contexts/WordReducer.js
@@ -1,9 +1,23 @@
 export function reducer(state, action) {
     switch (action.type) {
-        case 'increment':
-            return state + 1;
-        case 'decrement':
-            return state - 1;
+        case 'TOGGLE_MENU':
+            return { ...state, isExpanded: !state.isExpanded };
+        case 'SET_WORD':
+            return { ...state, word: action.payload };
+        case 'GENERATE_SENTENCES':
+            return {
+                ...state,
+                sentences: [
+                    `I will ${state.word} some information to evaluate the potential.`,
+                    `I'm currently, ${state.word}ing how to communicate.`,
+
+
+
+
+
+                    `We ${state.word} abounding class, We use collaborate ${state.word} carr, We have got a family-acting carr, This makes me findings`,
+                ],
+            };
         default:
             throw new Error();
     }
